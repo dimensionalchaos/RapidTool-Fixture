69d26d179fb19f5f0e81f4e8b3047446f7c71d66
Raja Singh Sohal <90750149+dimensionalchaos@users.noreply.github.com>
2026-01-07 12:51:40 +0530

    Revert "commit"
    This reverts commit fdeeacfa899abdb70121e93eb2d11f1453827913.

--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -15,7 +15,7 @@ class ApiClient {
 
   constructor() {
     this.client = axios.create({
-      baseURL: `${API_URL}`,
+      baseURL: `${API_URL}/api`,
       timeout: API_TIMEOUT,
       headers: {
         'Content-Type': 'application/json',
